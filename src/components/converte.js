e81583d5643eb09247a6e47ce819fdc32eba3f55
Victor Fuly <victor_hugo_fuly@hotmail.com>
2020-07-05 16:32:22 -0300

    Conversor v1.1

--- src/components/converte.js
+++ src/components/converte.js
@@ -1,104 +1,90 @@
-import React, {Component} from 'react'
+
+
+import React, {useState, useEffect} from 'react'
 import './converte.css'
 
-async function getCoins(){
-    let response= await fetch(`https://free.currconv.com/api/v7/countries?apiKey=bcf924aaf59d14e3a53c`)
-    let data = await response.json()
-    return data;
-}
+
+const Converte = (props) =>{
+    const  [currencyA_value, setCurrencyA_value]= useState('');
+    const  [currencyB_value, setCurrencyB_value]= useState('0');
+    const  [currencyA, setCurrencyA]= useState('BRL');
+    const  [currencyB, setCurrencyB]= useState('USD');
+    const  [currencies, setCurrencies]= useState([
+        {name:'BRL',currencySymbol:'R$'},
+        {name:'USD',currencySymbol:'$'},
+        {name:'CAD',currencySymbol:'$'},
+        {name:'EUR',currencySymbol:'€'},
+        {name:"JPY",currencySymbol:"¥"}
+    ]);
+    const  [currencySymbolB, setCurrencySymbolB]= useState('$');
   
-
-export default class Converte extends Component{
-    constructor (props){
-        super(props);
-
-        this.state={
-            currencyA_value:'',
-            currencyB_value:'Valor convertido',
-            currencyA:'BRL',
-            currencyB:'USD',
-            currencies:[{name:'BRL',currencySymbol:'R$'},{name:'USD',currencySymbol:'$'},{name:'CAD',currencySymbol:'$'},{name:'EUR',currencySymbol:'€'},{name:"JPY",currencySymbol:"¥"}],
-            currencySymbolB:'$',
-          
+    const handleSelect =(e) =>{
+        if (e.target.name==='currencyA'){
+            setCurrencyA(e.target.value)
+        }else{
+            setCurrencyB(e.target.value)
         }
-        this.convert = this.convert.bind(this);
-    }
-    handleSelect =(e) =>{
-        
-        this.setState({
-            [e.target.name]: e.target.value
-            
-        },
-        this.convert,
-        );
-
-        
+        convert()
     }
  
     
-    handleSwap = (e) =>{
-        const currencyA= this.state.currencyA
-        const currencyB= this.state.currencyB
+    const handleSwap = (e) =>{
+        const currency_A= currencyA
+        const currency_B= currencyB
 
         e.preventDefault();
         
-        this.setState({
-            currencyA: currencyB,
-            currencyB : currencyA,
-
-        },
-        this.convert,
-        );
+        setCurrencyA(currency_B)
+        setCurrencyB(currency_A)
+        convert()
+        
        
 
     }
-    convert(){
-        let from_to = `${this.state.currencyA}_${this.state.currencyB}`
+    const convert=() =>{
+        let from_to = `${currencyA}_${currencyB}`
         let url = `https://free.currconv.com/api/v7/convert?q=${from_to}&compact=ultra&apiKey=bcf924aaf59d14e3a53c`
         fetch(url).then(res=>{
           return res.json()
         })
         .then(json=>{
           let price = json[from_to]
-          let currencyB_value = (parseFloat(this.state.currencyA_value*price)).toFixed(2)
-          this.setState({currencyB_value})
+          let currencyB_value = (parseFloat(currencyA_value*price)).toFixed(2)
+          setCurrencyB_value(currencyB_value)
         })
        
-        let  symbol =  this.state.currencies.find(currency => currency.name === this.state.currencyB)
-        
-        this.setState({
-            currencySymbolB:symbol.currencySymbol
-
-        })
+        let  symbol =  currencies.find(currency => currency.name === currencyB)
+        setCurrencySymbolB(symbol.currencySymbol)
+      
     }
 
     
 
 
-    render(){
+    
         return (
             <div className="converte">
                 <h1>Conversor de moedas</h1>
-                <h2>{this.state.currencyA} para {this.state.currencyB}</h2>
+                <h2>{currencyA} para {currencyB}</h2>
                 <h2><select name="currencyA" 
-                Value ={this.state.currencyA}
-                onChange ={this.handleSelect}
-                onFocus ={this.handleSelect}>
-                    {this.state.currencies.map((currency)=>
+                value ={currencyA}
+                onChange ={handleSelect}
+                onFocus ={handleSelect}>
+                    {currencies.map((currency)=>
                     <option key={currency.name} value={currency.name}>  
                     {currency.name}
                     </option>
                     )}
                     
-                </select> 
-                <p>
-                <h1 className="swap" onClick={this.handleSwap}>&#8595;&#8593;</h1>
-                </p>
-                <select name="currencyB" 
-                Value ={this.state.currencyB}
-                onChange ={this.handleSelect}
-                onFocus ={this.handleSelect}>
-                    {this.state.currencies.map((currency)=>
+                </select> </h2>
+                
+                <h1 className="swap" onClick={handleSwap}>&#8595;&#8593;</h1>
+                
+                <h2> <select name="currencyB" 
+                value ={currencyB}
+                onChange ={handleSelect}
+                onFocus ={handleSelect}>
+                    {currencies.map((currency)=>
                     <option key={currency.name} value={currency.name}>  
                     {currency.name}
                     </option>
@@ -107,13 +93,14 @@ export default class Converte extends Component{
                 </select> </h2>
                 
                 
-                <input type="text" placeholder="Valor"onChange={(event)=>{this.setState({currencyA_value:event.target.value})}}/>
-                <input type="button" value="Converter" onClick={this.convert}/>
-                    <h2>{this.state.currencySymbolB}  {this.state.currencyB_value}</h2>
+                <input type="text" placeholder="Valor"onChange={(event)=>setCurrencyA_value(event.target.value)}/>
+                <input type="button" value="Converter" onClick={convert}/>
+                    <h2>{currencySymbolB}  {currencyB_value}</h2>
             </div>
         )
 
-    }
+    
 
    
 }
+export default Converte;
